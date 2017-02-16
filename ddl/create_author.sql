d46c2c8fc22dbc65a58500908579bbc476dd3922
cpmacbook <christopher.hale.pearce@gmail.com>
2017-02-16 12:23:37 -0500

    completed first 3 sql done

---
+++ ddl/create_author.sql
@@ -0,0 +1,10 @@
+CREATE TABLE author(
+  aid int NOT NULL,
+  name VARCHAR(200) NOT NULL ,
+  address VARCHAR(200) NOT NULL,
+  PRIMARY KEY (aid),
+  FOREIGN KEY fk_people(name, address)
+  REFERENCES people(name, address)
+  ON UPDATE CASCADE
+  ON DELETE RESTRICT
+);
