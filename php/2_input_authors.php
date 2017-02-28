5f85fb6de655866b32c62a57e87df21e655ced20
Jason Downing <jdown1994@gmail.com>
2017-02-28 12:04:28 -0500

    query 2 looks good

--- php/2_input_authors.php
+++ php/2_input_authors.php
@@ -1,14 +1,26 @@
 // User input one author name, find all the books written by the author(s).
 
 // This will get the author's aid. 'Dan Brown' is a placeholder.
-SELECT author.aid FROM author WHERE author.name = 'Dan Brown'
+SELECT author.aid
+FROM author
+WHERE author.name = 'Dan Brown'
 
 // This would equal: 1000000004
 
 // Now we can use this aid in a query on the writes table
-SELECT ISBN13 FROM writes WHERE writes.aid = '1000000004'
+SELECT ISBN13
+FROM writes
+WHERE writes.aid = '1000000004'
 
 // Now we have all the ISBN's that author has written. Let's find all their
-SELECT * FROM `bookdb`.`book` WHERE `ISBN13` = 9780385537858
+SELECT *
+FROM book
+WHERE `ISBN13` = 9780385537858
 
-// Now we have all the info
+// Now we have all the info!
+9780385537858
+Inferno
+2013-05-14
+mystery thriller
+Doubleday
+4.99
