9f1b5ecae92b969a522ff6c3e378d53c2d3207f8
Jason Downing <jdown1994@gmail.com>
2017-03-03 13:50:25 -0500

    Trying out some SQL queries

--- php/2_input_authors.php
+++ php/2_input_authors.php
@@ -11,24 +11,6 @@
  */
 
 /**
- * // This will get the author's aid. 'Dan Brown' is a placeholder.
- * SELECT author.aid
- * FROM author
- * WHERE author.name = 'Dan Brown'
- *
- * // This would equal: 1000000004
- *
- * // Now we can use this aid in a query on the writes table
- * SELECT ISBN13
- * FROM writes
- * WHERE writes.aid = '1000000004'
- *
- * // Now we have all the ISBN's that author has written. Let's find all their
- * SELECT *
- * FROM book
- * WHERE `ISBN13` = 9780385537858
- *
- *
  * // Joining all these queries we get:
  *
  * SELECT author.aid
@@ -64,17 +46,60 @@ if (!$my_conn) {
     exit;
 }
 
+/**
+* // This will get the author's aid. 'Dan Brown' is a placeholder.
+* SELECT author.aid
+* FROM author
+* WHERE author.name = 'Dan Brown'
+    *
+ * // This would equal: 1000000004
+ *
+ * // Now we can use this aid in a query on the writes table
+ * SELECT ISBN13
+* FROM writes
+* WHERE writes.aid = '1000000004'
+    *
+ * // Now we have all the ISBN's that author has written. Let's find all their
+ * SELECT *
+ * FROM book
+ * WHERE `ISBN13` = 9780385537858
+*/
+
+// First get AID
 $query = "SELECT author.aid
-            FROM author
-            WHERE author.name = '" . $author_name . "'";
+          FROM author
+          WHERE author.name = '" . $author_name . "'";
 $result = mysqli_query($my_conn,$query) or die (mysqli_error($my_conn) . 'Query failed: ');
 
 echo 'Aid<br>';
 
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    echo $row["aid"];
+    $aid = $row["aid"];
+    echo $aid;
     echo '<br>';
 }
+
+echo '<br> <br>';
+
+// Now use aid to get ISBN13
+$query = "SELECT ISBN13
+          FROM writes
+          WHERE writes.aid = '" . $aid . "'";
+$result = mysqli_query($my_conn,$query) or die (mysqli_error($my_conn) . 'Query failed: ');
+
+echo 'ISBN13<br>';
+
+$ISBN13_array = array();    // make a new array for ISBN13 data
+$index = 0;
+
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    $ISBN13_array[$index] = $row;
+    echo $ISBN13_array[$index];
+    echo '<br>';
+    $index++;
+}
+
+// Now we have an array of ISBN13 data, so let's get the book data now!
 
 mysqli_free_result($result);
 mysqli_close($my_conn);
