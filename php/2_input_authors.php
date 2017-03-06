62c673c250526ddfaa6385377d69c3b87965da66
Jason Downing <jdown1994@gmail.com>
2017-03-06 12:32:33 -0500

    Query 2 is simpler now.

--- php/2_input_authors.php
+++ php/2_input_authors.php
@@ -22,29 +22,20 @@ if (!$my_conn) {
     exit;
 }
 
-/* Find all the book's that the given author has written.
-
+/*  Find all the book's that the given author has written.
+    This uses two sub queries, as we need to get the author's aid, then get all the book's they have written
+    (by getting each book's ISBN13 number) and then getting all the data on each of those books.
 */
-$query = "SELECT ISBN13
-          FROM writes w JOIN author a
-          ON w.aid = a.aid
-          WHERE w.aid IN (SELECT author.aid
-                          FROM author
-                          WHERE author.name = '" . $author_name . "')";
+$query = "SELECT *
+          FROM book b JOIN writes w
+          ON b.ISBN13 = w.ISBN13
+          WHERE b.ISBN13 IN (SELECT ISBN13
+                                FROM writes w JOIN author a
+                                ON w.aid = a.aid
+                                WHERE w.aid IN (SELECT author.aid
+                                                FROM author
+                                                WHERE author.name = '" . $author_name . "'))";
 $result = mysqli_query($my_conn, $query) or die (mysqli_error($my_conn) . 'Query failed: ');
-
-echo '<b>ISBN13</b><br>';
-
-$ISBN13_array = array();    // make a new array for ISBN13 data
-$index = 0;
-
-// Testing output of the ISBN's we found
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    $ISBN13_array[$index] = $row["ISBN13"];
-    echo $ISBN13_array[$index];
-    echo '<br>';
-    $index++;
-}
 
 // Results table
 echo "<br><table>";
@@ -54,29 +45,15 @@ echo "<tr> <td><b>title</b></td>
 <td><b>publisher</b></td> 
 <td><b>price</b></td></tr>";
 
-/*  Now we have all the ISBN's that author has written. Let's find all the books they wrote.
-    We'll put this into an array of ISBN13 data.  */
-for($i = 0; $i < sizeof($ISBN13_array); $i++) {
-    $query = "SELECT *
-              FROM book
-              WHERE book.ISBN13 = '" . $ISBN13_array[$i] . "'";
-    $result = mysqli_query($my_conn,$query) or die (mysqli_error($my_conn) . 'Query failed: ');
-
-    // See if the query failed
-    if (mysqli_num_rows($result) == 0) {
-        echo "Query failed, couldn't find any books for ISBN13: $ISBN13_array[$i]! <br>";
-        return 0;
-    }
-
-    // If the query worked, we can print out all the info for this book! Includes: title, year, category, pname, price
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        echo "<tr><td>" . $row["title"] . "</td>";
-        echo "<td>" . $row["year"] . "</td>";
-        echo "<td>" . $row["category"] . "</td>";
-        echo "<td>" . $row["pname"] . "</td>";
-        echo "<td>" . $row["price"] . "</td>";
-        echo '</tr>';
-    }
+// If the query worked, we can print out all the info for this book!
+// Includes: title, year, category, pname, price
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    echo "<tr><td>" . $row["title"] . "</td>";
+    echo "<td>" . $row["year"] . "</td>";
+    echo "<td>" . $row["category"] . "</td>";
+    echo "<td>" . $row["pname"] . "</td>";
+    echo "<td>" . $row["price"] . "</td>";
+    echo '</tr>';
 }
 
 echo '</table>';        // End of results table
